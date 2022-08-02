09352091b2bf49378a8400b64b0140ef6898a0e6
Makktu <johnmcna@gmail.com>
2022-08-02 19:04:09 +0100

    abandoned

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-"use strict";
+'use strict';
+
+const emailField = document.getElementById('email');
+
+emailField.addEventListener('blur', (event) => {
+    if (!emailField.checkValidity()) {
+        document.getElementById('msgArea').innerHTML =
+            emailField.validationMessage;
+    }
+});
